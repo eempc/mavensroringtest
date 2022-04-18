16b186547b20c4822a3fc0bcee51242d68b1b122
Evelyn <evc776@gmail.com>
2022-04-18 09:23:17 +0100

    uncommented some code

--- src/main/java/com/example/meowmaven2/config/AppConfig.java
+++ src/main/java/com/example/meowmaven2/config/AppConfig.java
@@ -36,30 +36,31 @@ public class AppConfig {
     // @Bean should replace wherever you use New
     // Everything below can be removed if you set the SpeakerServiceImpl class to @Service
     // SCOPE_SINGLETON will give you singleton, SCOPE_PROTOTYPE will generate a new instance of this speaker service
-//    @Bean(name = "speakerService") // Optional name. Beans for methods only, not class
-//    @Scope(value = BeanDefinition.SCOPE_SINGLETON) //@Scope(value = BeanDefinition.SCOPE_PROTOTYPE) // Alternative string @Scope(value = "singleton")
-//    public SpeakerService getSpeakerService() {
-//        // 1. Java version
-//        //return new SpeakerServiceImpl();
-//
-//        // 2. Setter injection using a setRepository
-//        //SpeakerServiceImpl service = new SpeakerServiceImpl();
-//        //service.setRepository(getSpeakerRepository());
-//
-//        // 3. Constructor injection of the singleton, which is a Hibernate repository
-//        // Notice how the service is separate from the repository
-//        // The repository is the "database" where the data is hard-coded in this example but could be the SQL
-//        // The service serves up the repository, the abstraction between these 2 concepts eludes me
-//        // Why this speaker service and its repository is not a single class?
-//        //SpeakerServiceImpl service = new SpeakerServiceImpl(getSpeakerRepository());
-//        SpeakerServiceImpl service = new SpeakerServiceImpl();
-//        return service;
-//    }
+    @Bean(name = "speakerService") // Optional name. Beans for methods only, not class
+    @Scope(value = BeanDefinition.SCOPE_SINGLETON) //@Scope(value = BeanDefinition.SCOPE_PROTOTYPE) // Alternative string @Scope(value = "singleton")
+    public SpeakerService getSpeakerService() {
+        // 1. Java version
+        //return new SpeakerServiceImpl();
+
+        // 2. Setter injection using a setRepository
+        //SpeakerServiceImpl service = new SpeakerServiceImpl();
+        //service.setRepository(getSpeakerRepository());
+
+        // 3. Constructor injection of the singleton, which is a Hibernate repository
+        // Notice how the service is separate from the repository
+        // The repository is the "database" where the data is hard-coded in this example but could be the SQL
+        // The service serves up the repository, the abstraction between these 2 concepts eludes me
+        // Why this speaker service and its repository is not a single class?
+        //SpeakerServiceImpl service = new SpeakerServiceImpl(getSpeakerRepository());
+
+        SpeakerServiceImpl service = new SpeakerServiceImpl();
+        return service;
+    }
 
     // This can be removed as well if HibernateRepository is set up as @Repository
     // Singleton for getting the repository
-//    @Bean(name = "speakerRepository")
-//    public SpeakerRepository getSpeakerRepository() {
-//        return new HibernateSpeakerRepositoryImpl();
-//    }
+    @Bean(name = "speakerRepository")
+    public SpeakerRepository getSpeakerRepository() {
+        return new HibernateSpeakerRepositoryImpl();
+    }
 }
